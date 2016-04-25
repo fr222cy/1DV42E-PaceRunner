567772ceea2fe0a542a735d8da02a944da2fa6c9
fr222cy <fr222cy@student.lnu.se>
2016-04-25 17:02:45 +0200

    Fix: More reliable GPS and Network precision

--- java/se/filiprydberg/pacerunner/pacerunner.java
+++ java/se/filiprydberg/pacerunner/pacerunner.java
@@ -1,9 +1,14 @@
 package se.filiprydberg.pacerunner;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,10 +16,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
-public class pacerunner extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+
+public class pacerunner extends AppCompatActivity implements OnMapReadyCallback {
+    private Location _location;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
     private Button proceedButton;
-
+    private GoogleMap map;
+    private MarkerOptions userPositionMarker;
+    private Marker marker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,16 +40,29 @@ public class pacerunner extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        userPositionMarker = new MarkerOptions()
+                .position(new LatLng(0, 0));
+
+
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+
         proceedButton = (Button) findViewById(R.id.proceedButton);
 
-        proceedButton.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(getApplicationContext(), setPaceActivity.class);
+        proceedButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SetPace.class);
                 startActivity(intent);
             }
         });
+
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
 
     }
 
@@ -43,7 +74,8 @@ public class pacerunner extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -56,4 +88,74 @@ public class pacerunner extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        marker = map.addMarker(userPositionMarker);
+        RetrieveLocation();
+    }
+
+    public void RetrieveLocation(){
+         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+         locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                    System.out.println("im inside:" + location);
+                    LatLng currentPos = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    marker.setPosition(currentPos);
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentPos, 17));
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET,Manifest.permission.ACCESS_NETWORK_STATE
+                }, 10);
+
+            }
+        } else {
+
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch(requestCode){
+            case 10:
+                if (grantResults.length>0 && grantResults[0]== PackageManager.PERMISSION_GRANTED)
+                    locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    public Location getPosition() {
+        if(_location != null){
+            return _location;
+        }
+        else{
+            return null;
+        }
+
+    }
 }
+
