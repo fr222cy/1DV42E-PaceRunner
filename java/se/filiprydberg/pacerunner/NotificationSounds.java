088daa7940d59adfafe58d489e6a16234203b87a
fr222cy <fr222cy@student.lnu.se>
2016-05-10 16:55:07 +0200

    Sounds

---
+++ java/se/filiprydberg/pacerunner/NotificationSounds.java
@@ -0,0 +1,19 @@
+package se.filiprydberg.pacerunner;
+
+/**
+ * Created by Filip on 5/9/2016.
+ */
+public class NotificationSounds {
+
+    public String[] getNotificationArray() {
+        String[] notification = new String[]{
+                "Jaws",
+                "Psycho",
+                "Chased by Dogs",
+                "Beep",
+                "Fishmouth"};
+
+        return notification;
+    }
+
+}
