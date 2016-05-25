e83bc151e2b8a52a7b5ddae45fdf3593124d5e12
fr222cy <fr222cy@student.lnu.se>
2016-05-25 21:06:31 +0200

    2 new methods. One for setting the sounds based on users choice. And one for playing them.

--- java/se/filiprydberg/pacerunner/NotificationSounds.java
+++ java/se/filiprydberg/pacerunner/NotificationSounds.java
@@ -1,19 +1,68 @@
 package se.filiprydberg.pacerunner;
 
+import android.content.Context;
+import android.media.MediaPlayer;
+
 /**
  * Created by Filip on 5/9/2016.
  */
 public class NotificationSounds {
-
+    private String[] notification = new String[]{
+            "Jaws",
+            "Psycho",
+            "Chased by Dogs",
+            "Beep",
+            "Fishmouth"};
+    private MediaPlayer firstSound;
+    private MediaPlayer secondSound;
+    private int previousSoundType;
     public String[] getNotificationArray() {
-        String[] notification = new String[]{
-                "Jaws",
-                "Psycho",
-                "Chased by Dogs",
-                "Beep",
-                "Fishmouth"};
-
         return notification;
     }
+
+    public void setMediaPlayerSounds(String soundName, Context context){
+        int resourceIdForFirstSound = context.getResources().getIdentifier(
+                soundName.toLowerCase() + 1, "raw", context.getPackageName());
+
+        firstSound = MediaPlayer.create(context, resourceIdForFirstSound);
+        firstSound.setLooping(true);
+
+        int resourceIdForSecondSound = context.getResources().getIdentifier(
+                soundName.toLowerCase() + 2, "raw", context.getPackageName());
+
+        secondSound = MediaPlayer.create(context, resourceIdForSecondSound);
+        secondSound.setLooping(true);
+    }
+
+    public void playSoundByType(int soundType){
+
+        if(previousSoundType == soundType){
+            return;
+        }
+
+        if(soundType == 1){
+            if(secondSound.isPlaying()){
+                secondSound.stop();
+            }
+            firstSound.start();
+        }
+
+        if(soundType == 2){
+            if(firstSound.isPlaying()){
+                firstSound.stop();
+            }
+            secondSound.start();
+        }
+        if(soundType == 0){
+            if(firstSound.isPlaying() || secondSound.isPlaying()){
+                firstSound.stop();
+                secondSound.stop();
+            }
+        }
+
+        previousSoundType = soundType;
+
+    }
+
 
 }
