aa8bdb0f755784f9ab229024211b9b803fff7ac8
fr222cy <fr222cy@student.lnu.se>
2016-05-27 00:06:31 +0200

    Changed from MediaPlayer to SoundPool, since that was a better option for short sounds.

--- java/se/filiprydberg/pacerunner/NotificationSounds.java
+++ java/se/filiprydberg/pacerunner/NotificationSounds.java
@@ -1,7 +1,8 @@
 package se.filiprydberg.pacerunner;
 
 import android.content.Context;
-import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.media.SoundPool;
 
 /**
  * Created by Filip on 5/9/2016.
@@ -10,58 +11,63 @@ public class NotificationSounds {
     private String[] notification = new String[]{
             "Jaws",
             "Psycho",
-            "Chased by Dogs",
-            "Beep",
-            "Fishmouth"};
-    private MediaPlayer firstSound;
-    private MediaPlayer secondSound;
+            "Dogs",
+            "Beep"};
+    SoundPool sp;
+    private int firstSound;
+    private int secondSound;
+    private int firstSoundID;
+    private int secondSoundID;
     private int previousSoundType;
     public String[] getNotificationArray() {
         return notification;
     }
-
+    //Retrieve the sound files from the resource.
+    //Creates a Soundpool and load the files. That are used in playSoundByType()
     public void setMediaPlayerSounds(String soundName, Context context){
+
         int resourceIdForFirstSound = context.getResources().getIdentifier(
-                soundName.toLowerCase() + 1, "raw", context.getPackageName());
-
-        firstSound = MediaPlayer.create(context, resourceIdForFirstSound);
-        firstSound.setLooping(true);
-
+                soundName.toLowerCase() + "1", "raw", context.getPackageName());
         int resourceIdForSecondSound = context.getResources().getIdentifier(
-                soundName.toLowerCase() + 2, "raw", context.getPackageName());
+                soundName.toLowerCase() + "2", "raw", context.getPackageName());
 
-        secondSound = MediaPlayer.create(context, resourceIdForSecondSound);
-        secondSound.setLooping(true);
+        sp = new SoundPool(1, AudioManager.STREAM_MUSIC,0);
+        firstSound = sp.load(context, resourceIdForFirstSound, 1);
+        secondSound = sp.load(context, resourceIdForSecondSound,1);
     }
-
+    //Plays a sound, based on the gap to the users average pace.
     public void playSoundByType(int soundType){
 
         if(previousSoundType == soundType){
             return;
         }
-
-        if(soundType == 1){
-            if(secondSound.isPlaying()){
-                secondSound.stop();
+        try {
+            switch (soundType){
+                case 1: {
+                    if(firstSound != 0)
+                        firstSoundID = sp.play(firstSound,1,1,1,-1,1);
+                        break;
+                }
+                case 2: {
+                    if(secondSound != 0)
+                        secondSoundID = sp.play(secondSound,1,1,1,-1,1);
+                        break;
+                }
+                case 0: {
+                    if(firstSoundID != 0){
+                        sp.stop(firstSoundID);
+                    }
+                    if(secondSoundID != 0){
+                        sp.stop(secondSoundID);
+                    }
+                    break;
+                }
             }
-            firstSound.start();
-        }
-
-        if(soundType == 2){
-            if(firstSound.isPlaying()){
-                firstSound.stop();
-            }
-            secondSound.start();
-        }
-        if(soundType == 0){
-            if(firstSound.isPlaying() || secondSound.isPlaying()){
-                firstSound.stop();
-                secondSound.stop();
-            }
+        }catch (Exception e){
+            e.printStackTrace();
         }
 
         previousSoundType = soundType;
-
     }
 
 
