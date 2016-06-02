362e9833bcbac90195f9ac63e6e585fa8d75c1bd
fr222cy <fr222cy@student.lnu.se>
2016-06-02 21:32:04 +0200

    SummaryActivity, empty class

---
+++ java/se/filiprydberg/pacerunner/Activities/SummaryActivity.java
@@ -0,0 +1,24 @@
+package se.filiprydberg.pacerunner.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import se.filiprydberg.pacerunner.R;
+
+public class SummaryActivity extends AppCompatActivity {
+    //TODO: Implement Class
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
